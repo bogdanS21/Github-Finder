9e74b090c4992f353e23507c76af8b3ea06908a1
Bogdan Seliman <selimanbogdan@gmail.com>
2022-04-02 20:19:53 +0300

    UserProfile

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -10,6 +10,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
     const initialeState = {
         users: [],
+        user: {},
         loading: false
     }
     const [state, dispatch] = useReducer(githubReducer, initialeState)
@@ -35,15 +36,42 @@ export const GithubProvider = ({ children }) => {
         })
 
     }
+
+    //Search single user
+    const getUser = async (login) => {
+        setLoading()
+
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+        if (response.status === 404) {
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+            console.log(data)
+
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+
+
+    }
     const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
     const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
     return <GithubContext.Provider value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser
 
     }}>
         {children}

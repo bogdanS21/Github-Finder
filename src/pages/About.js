364ac3345dae7e4688836660e575a1a1ec92cfa8
Bogdan Seliman <selimanbogdan@gmail.com>
2022-04-03 14:00:02 +0300

    Final version

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
             <p className="mb-4 text-2xl font-light">
                 A React app to search Github profiles and see profile details.This project is part of the <a href="https://www.udemy.com/course/modern-react-front-to-back">
                     {''}React Front To Back
-                </a>{''} Udemy course.
+                </a>{''} Udemy course .
             </p>
             <p className="text-lg text-gray-400">
                 Version <span className="text-white">1.0.0</span>
